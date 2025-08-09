36bd556fe08e0f84e4d47462655fa896d9b5f576
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-08 22:21:34 -0300

    Criando estrutura no node

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,1 +1,17 @@
-const http = require("http");
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usúarios");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usúarios");
+  }
+
+  return res.end("Hello World");
+});
+
+server.listen(3000, () => {});
