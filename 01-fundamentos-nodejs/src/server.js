4006e57d5adff7bafa57a19232fd77f00cac7115
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-08 22:35:54 -0300

    Entendendo os metodos Get y Post

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,17 +1,27 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usúarios");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("Criação de usúarios");
+    users.push({
+      id: 1,
+      name: "Bruno",
+      email: "b@b.com",
+    });
+
+    return res.end("Usuário criado com sucesso!");
   }
 
   return res.end("Hello World");
 });
 
-server.listen(3000, () => {});
+server.listen(3000);
