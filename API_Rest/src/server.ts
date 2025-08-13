9b5c9ca5bd6fad7a8f71427f109eccb970f8f32e
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-13 19:44:54 -0300

    Adicionando o typescript e dependencias

--- API_Rest/src/server.ts
+++ API_Rest/src/server.ts
@@ -1,11 +1,11 @@
-interface User {
-  birthYear: number;
-}
+import fastify from "fastify";
 
-function calculateAgeOfUser(user: User) {
-  return new Date().getFullYear() - user.birthYear;
-}
+const app = fastify();
 
-calculateAgeOfUser({
-  birthYear: 1988,
+app.get("/hello", () => {
+  return "Hello, World!";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server Running!");
 });
