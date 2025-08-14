efad2e2a34ae1fcdcb44cb0dd1f21e74c0503b8d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-14 19:01:07 -0300

    Trantando env com zod

--- API_Rest/src/server.ts
+++ API_Rest/src/server.ts
@@ -1,18 +1,15 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'New Transaction',
-    amount: 1000,
-  })
-  return { transaction }
+  const transactions = await knex('transactions').where('amount', 1000).select('*')
+
+  return transactions
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 })
