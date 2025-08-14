1134af9a312003bfe1db004dc8b0d20c39855152
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-14 18:13:38 -0300

    criando queries com knex

--- API_Rest/src/server.ts
+++ API_Rest/src/server.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'New Transaction',
+    amount: 1000,
+  })
+  return { transaction }
 })
 
 app.listen({ port: 3333 }).then(() => {
