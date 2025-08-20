942b65e3dd5950f091439131e4e22588e6beb74b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-20 20:59:04 -0300

    Integraçao com Prisma ORM - Postgresql com Docker

--- 03-API-SOLID/src/app.ts
+++ 03-API-SOLID/src/app.ts
@@ -1,5 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from './generated/prisma/index.js'
 
 export const app = fastify({})
 
 // ORM - Object Relational Mapping
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'John Doe',
+    email: 'john@doe',
+  },
+})
