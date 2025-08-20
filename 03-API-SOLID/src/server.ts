2892b3e51898f68aed25acadf35b703a42657bcd
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-20 17:06:54 -0300

    Estrtura do projeto - Criando aliases de importaçao

--- 03-API-SOLID/src/server.ts
+++ 03-API-SOLID/src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app.js'
-import { env } from './env/index.js'
+import { env } from '@/env/index.js'
 
 app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
   console.log('🚀HTTP Server Running!')
