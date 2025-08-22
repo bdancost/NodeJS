99d08852885ae1d119f559947fead39bea99c566
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-21 21:07:26 -0300

    Integraçao com Prisma ORM - Criando schema do Prisma

--- 03-API-SOLID/generated/prisma/index-browser.js
+++ 03-API-SOLID/generated/prisma/index-browser.js
@@ -123,7 +123,25 @@ exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
 exports.Prisma.UserScalarFieldEnum = {
   id: 'id',
   name: 'name',
-  email: 'email'
+  email: 'email',
+  password_hash: 'password_hash',
+  created_at: 'created_at',
+  updated_at: 'updated_at'
+};
+
+exports.Prisma.CheckInScalarFieldEnum = {
+  id: 'id',
+  created_at: 'created_at',
+  validate_at: 'validate_at'
+};
+
+exports.Prisma.GymScalarFieldEnum = {
+  id: 'id',
+  title: 'title',
+  description: 'description',
+  phone: 'phone',
+  latitude: 'latitude',
+  longitude: 'longitude'
 };
 
 exports.Prisma.SortOrder = {
@@ -136,9 +154,16 @@ exports.Prisma.QueryMode = {
   insensitive: 'insensitive'
 };
 
+exports.Prisma.NullsOrder = {
+  first: 'first',
+  last: 'last'
+};
+
 
 exports.Prisma.ModelName = {
-  User: 'User'
+  User: 'User',
+  CheckIn: 'CheckIn',
+  Gym: 'Gym'
 };
 
 /**
