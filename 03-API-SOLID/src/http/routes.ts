e42c1bad90a304f0282da2a7eec3597a3c56d6ec
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-23 20:18:41 -0300

    Caso de uso e design patterns - controller de registro

---
+++ 03-API-SOLID/src/http/routes.ts
@@ -0,0 +1,6 @@
+import type { FastifyInstance } from 'fastify'
+import { register } from '@/http/controllers/register.js'
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+}
