c62a5879a7fa3a894a1da5eea4ea1978b3f60695
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-24 17:14:26 -0300

    Design Patterns & Testes - Primeiro teste unitário

--- 03-API-SOLID/src/use-cases/register.ts
+++ 03-API-SOLID/src/use-cases/register.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-
+import type { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import type { UsersRepository } from '@/repositories/users-repository.js'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
@@ -10,13 +10,21 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 // SOLID
 // D - Dependency Inversion Principle
 
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -25,10 +33,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
