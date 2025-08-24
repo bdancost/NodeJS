dca4dc2fc480a4f02e336ed297bf0dae49a85c9d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-24 10:02:27 -0300

    Caso de Uso e Design Patterns - Lidando com erros do uso case

--- 03-API-SOLID/src/use-cases/register.ts
+++ 03-API-SOLID/src/use-cases/register.ts
@@ -2,6 +2,7 @@
 
 import { hash } from 'bcryptjs'
 import type { UsersRepository } from '@/repositories/users-repository.js'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -21,7 +22,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('User already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
