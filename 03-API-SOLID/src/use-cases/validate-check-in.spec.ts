0123f634d5a2a1fc3b5b4c9e5f58056539f83d47
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-31 00:04:33 -0300

    Implementando casos de uso - Caso de uso de validar de check-in

--- 03-API-SOLID/src/use-cases/validate-check-in.spec.ts
+++ 03-API-SOLID/src/use-cases/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository.js'
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { ValidateCheckInUseCase } from '@/use-cases/validate-check-in.js'
 import { ResourceNotFoundError } from './errors/resource-not-found-error.js'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error.js'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -10,6 +11,12 @@ describe('Validate Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
   it('should be able to validate check-in', async () => {
     const createdCheckIn = await checkInsRepository.create({
@@ -32,5 +39,25 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+      validate_at: new Date(),
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
