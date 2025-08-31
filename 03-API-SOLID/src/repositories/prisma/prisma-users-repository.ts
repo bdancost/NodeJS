bf22a17767a0655a4b782392ef5bd24291c82444
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-08-31 19:57:11 -0300

    Controllers & Testes E2E - Repositorio do Prisma

--- 03-API-SOLID/src/repositories/prisma/prisma-users-repository.ts
+++ 03-API-SOLID/src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,17 @@
 import { prisma } from '@/lib/prisma.js'
 import type { Prisma } from '@prisma/client'
-import type { User } from '@prisma/client'
 
 import type { UsersRepository } from '@/repositories/users-repository.js'
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(_id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
