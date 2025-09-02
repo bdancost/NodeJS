ed091ec0fe80bcd0cd2245bbc02c1e93e8ebde4c
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-02 16:24:44 -0300

    Principio de autenticaçao JWT

--- 03-API-SOLID/src/http/routes.ts
+++ 03-API-SOLID/src/http/routes.ts
@@ -1,9 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from '@/http/controllers/register.js'
+import { profile } from './controllers/profile.js'
 import { authenticate } from './controllers/autheticate.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
