b1894e5ed0b7ece7dfd43c6f2f277acc522a1f48
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-04 20:56:29 -0300

    Adicionando test environment

---
+++ 03-API-SOLID/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest/environments'
+import { prisma } from '@/lib/prisma.js'
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = `test_${randomUUID()}`
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    console.log(databaseUrl)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma db push')
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        )
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
