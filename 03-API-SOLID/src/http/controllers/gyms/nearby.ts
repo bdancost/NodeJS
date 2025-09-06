7ace1a6d3351d45d42fb2057da0408ccf5c1d818
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-06 17:30:13 -0300

    Outros controllers da academia

---
+++ 03-API-SOLID/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case.js'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymBodySchema = z.object({
+    latitude: z.number().refine((val) => {
+      return Math.abs(val) >= -90 && val <= 90
+    }, 'Latitude must be between -90 and 90'),
+    longitude: z.number().refine((val) => {
+      return Math.abs(val) >= -180 && val <= 180
+    }, 'Longitude must be between -180 and 180'),
+  })
+
+  const { latitude, longitude } = nearbyGymBodySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send({ gyms })
+}
