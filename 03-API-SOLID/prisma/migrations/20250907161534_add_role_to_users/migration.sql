f8fefb14126e32e1bcf7dcb4d31682164a6b0445
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-07 14:26:44 -0300

    Autorizaçao por cargos (RBAC)

--- 03-API-SOLID/prisma/migrations/20250907161534_add_role_to_users/migration.sql
+++ 03-API-SOLID/prisma/migrations/20250907161534_add_role_to_users/migration.sql
@@ -1,24 +1,21 @@
 /*
   Warnings:
 
-  - You are about to drop the column `userId` on the `check_ins` table. All the data in the column will be lost.
   - You are about to drop the column `updated_at` on the `users` table. All the data in the column will be lost.
   - Added the required column `gym_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
   - Added the required column `user_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
-  - Made the column `validate_at` on table `check_ins` required. This step will fail if there are existing NULL values in that column.
 
 */
--- DropForeignKey
-ALTER TABLE "public"."check_ins" DROP CONSTRAINT "check_ins_userId_fkey";
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'MEMBER');
 
 -- AlterTable
-ALTER TABLE "public"."check_ins" DROP COLUMN "userId",
-ADD COLUMN     "gym_id" TEXT NOT NULL,
-ADD COLUMN     "user_id" TEXT NOT NULL,
-ALTER COLUMN "validate_at" SET NOT NULL;
+ALTER TABLE "public"."check_ins" ADD COLUMN     "gym_id" TEXT NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
 
 -- AlterTable
-ALTER TABLE "public"."users" DROP COLUMN "updated_at";
+ALTER TABLE "public"."users" DROP COLUMN "updated_at",
+ADD COLUMN     "role" "public"."Role" NOT NULL DEFAULT 'MEMBER';
 
 -- AddForeignKey
 ALTER TABLE "public"."check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
