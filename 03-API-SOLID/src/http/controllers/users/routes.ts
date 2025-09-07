cd829e70ed49e35bc51793ec634fb8addbc7c349
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-07 13:05:12 -0300

    Implementaçao do reflesh token

--- 03-API-SOLID/src/http/controllers/users/routes.ts
+++ 03-API-SOLID/src/http/controllers/users/routes.ts
@@ -2,12 +2,14 @@ import type { FastifyInstance } from 'fastify'
 import { register } from '@/http/controllers/users/register.js'
 import { profile } from './profile.js'
 import { authenticate } from './autheticate.js'
+import { reflesh } from './reflesh.js'
 import { verifyJWT } from '../../middlewares/verify-jwt.js'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', reflesh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
