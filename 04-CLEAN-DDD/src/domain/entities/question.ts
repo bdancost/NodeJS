119a52564e0b7495543915ea03e6d0c716ae82fb
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-19 14:32:19 -0300

    Abstraindo criaçao de entidades

--- 04-CLEAN-DDD/src/domain/entities/question.ts
+++ 04-CLEAN-DDD/src/domain/entities/question.ts
@@ -1,6 +1,7 @@
 import { Slug } from '../value-objects/slug.js'
 import { Entity } from '../../core/entities/entity.js'
 import { UniqueEntityID } from '../../core/entities/unique-entity-id.js'
+import type { Optional } from '../../core/types/optional.js'
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -11,4 +12,16 @@ interface QuestionProps {
   createdAt: Date
   updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+
+    return question
+  }
+}
