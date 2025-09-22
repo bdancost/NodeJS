58b8d225a090d97e27b37a47a5448b59032e6dd8
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-22 12:29:44 -0300

    Gerando dados ficticios

--- 04-CLEAN-DDD/test/factories/make-question.ts
+++ 04-CLEAN-DDD/test/factories/make-question.ts
@@ -1,16 +1,19 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
 import { Question } from '@/domain/forum/enterprise/entities/question.js'
 import type { QuestionProps } from '@/domain/forum/enterprise/entities/question.js'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug.js'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  )
 
   return question
 }
