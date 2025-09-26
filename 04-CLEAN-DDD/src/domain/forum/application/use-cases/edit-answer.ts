98ad7c4cf792d6ffcbe8151c63767e86a93e6381
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-26 15:05:16 -0300

    Caso de uso: editar resposta

---
+++ 04-CLEAN-DDD/src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,36 @@
+import { Answer } from '../../enterprise/entities/answer.js'
+import type { AnswersRepository } from '../repositories/answers-repository.js'
+
+interface EditAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface EditAnswerUseCaseResponse {
+  answer: Answer
+}
+
+export class EditAnswerUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({ answerId, authorId, content }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorID.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    answer.content = content
+
+    await this.answersRepository.save(answer)
+
+    return {
+      answer,
+    }
+  }
+}
