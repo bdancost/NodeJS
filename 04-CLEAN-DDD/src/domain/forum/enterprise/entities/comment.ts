a8e1053916ba94b4953cd728bc1e87e51c6786cb
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-01 20:20:16 -0300

    Classe base de comentarios

---
+++ 04-CLEAN-DDD/src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,34 @@
+import { Entity } from '@/core/entities/entity.js'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+
+export interface CommentProps {
+  authorId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export abstract class Comment<Props extends CommentProps> extends Entity<Props> {
+  get authorID() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+  }
+}
--- 04-CLEAN-DDD/src/domain/forum/enterprise/entities/question-comment.ts
+++ 04-CLEAN-DDD/src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,39 +1,15 @@
-import { Entity } from '@/core/entities/entity.js'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { Comment, type CommentProps } from './comment.js'
 import type { Optional } from '@/core/types/optional.js'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
-  answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
+export interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityID
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorID() {
-    return this.props.authorId
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
-
-  get content() {
-    return this.props.content
-  }
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-  }
-
   static create(props: Optional<QuestionCommentProps, 'createdAt'>, id?: UniqueEntityID) {
     const questionComment = new QuestionComment(
       {
