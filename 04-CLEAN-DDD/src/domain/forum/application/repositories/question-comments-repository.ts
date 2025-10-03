621697450a0db5c38ce2d2b4897b1f2cc1637ba0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-02 21:08:50 -0300

    Caso de uso: Comentar na pergunta

---
+++ 04-CLEAN-DDD/src/domain/forum/application/repositories/question-comments-repository.ts
@@ -0,0 +1,5 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment.js'
+
+export interface QuestionCommentsRepository {
+  create(questionComment: QuestionComment): Promise<void>
+}
