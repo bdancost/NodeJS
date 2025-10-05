aa1c9eb1c6042aafd1377e5ff64eae5b8ee11f9a
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-05 19:26:05 -0300

    Casos de uso: Deletar comentário da pergunta

---
+++ 04-CLEAN-DDD/test/factories/make-question-comment.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { QuestionComment, type QuestionCommentProps } from '@/domain/forum/enterprise/entities/question-comment.js'
+
+export function makeQuestionComment(override: Partial<QuestionCommentProps> = {}, id?: UniqueEntityID) {
+  const questionComment = QuestionComment.create(
+    {
+      authorId: new UniqueEntityID(),
+      questionId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  )
+
+  return questionComment
+}
