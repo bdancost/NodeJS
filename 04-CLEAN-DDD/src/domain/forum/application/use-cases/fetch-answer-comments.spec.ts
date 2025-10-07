fb773d53140520726d560a4dfe8e120fd4aebb18
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-07 16:41:02 -0300

    Caso de uso: Listar comentarios da resposta

---
+++ 04-CLEAN-DDD/src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,53 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository.js'
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments.js'
+import { makeAnswerComment } from 'test/factories/make-answer-comment.js'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Qustion Comments', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
+  })
+
+  it('should be able to fetch answer comments', async () => {
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      })
+    )
+
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      })
+    )
+
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      })
+    )
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 1,
+    })
+
+    expect(answerComments).toHaveLength(3)
+  })
+
+  it.skip('should be able to fetch pagination answers comments', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswerCommentsRepository.create(makeAnswerComment({ answerId: new UniqueEntityID('answer-1') }))
+    }
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 2,
+    })
+
+    expect(answerComments).toHaveLength(2)
+  })
+})
--- 04-CLEAN-DDD/test/repositories/in-memory-answer-comments-repository.ts
+++ 04-CLEAN-DDD/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,5 +1,6 @@
 import type { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository.js'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment.js'
+import type { PaginationParams } from '@/core/repositories/pagination-params.js'
 
 export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
   public items: AnswerComment[] = []
@@ -10,6 +11,14 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
       return null
     }
     return answerComment
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
