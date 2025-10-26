985b9e016b0a5e2bab759c617ffa35b59b6fc7e7
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-26 16:07:33 -0300

    Filtrando pelo status

--- 05-Support-tickets/src/controllers/tickets/index.js
+++ 05-Support-tickets/src/controllers/tickets/index.js
@@ -9,7 +9,9 @@
 export function index({ req, res, database }) {
   const { status } = req.query
 
-  const tickets = database.select('tickets')
+  const filters = status ? { status } : null
+
+  const tickets = database.select('tickets', filters)
 
   return res.end(JSON.stringify(tickets))
 }
