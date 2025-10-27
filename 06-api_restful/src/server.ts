f398541e2805f052b62a1c7dbbdafba79ef27c72
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-27 17:04:01 -0300

    Configurando e iniciando no express do node

--- 06-api_restful/src/server.ts
+++ 06-api_restful/src/server.ts
@@ -1,7 +1,25 @@
-function sum(a: number, b: number): number {
-  return a + b
-}
+import express from 'express'
+import { myMiddleware } from './middlewares/my-middleware'
 
-const result = sum(5, 3)
+const PORT = 3333
 
-console.log('Resultado da soma:', result)
+const app = express()
+app.use(express.json())
+
+app.use(myMiddleware)
+
+app.get('/products/', (request, response) => {
+  const { page, limit } = request.query
+  response.send(`Listagem de produtos ${page} e ${limit}`)
+})
+
+app.post('/products/', (request, response) => {
+  const { name, price } = request.body
+
+  response.status(201).json({
+    name,
+    price,
+  })
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
