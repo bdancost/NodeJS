94c8504d2edb5b65244922d8deb40dbc08621bc6
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-01 14:56:35 -0300

    Configurando o Knex.js

---
+++ 07-fullstack-query-builder/knexfile.ts
@@ -0,0 +1,11 @@
+export default {
+  client: 'sqlite3',
+  connection: {
+    filename: './src/database/database.db',
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './src/database/migrations',
+  },
+  useNullAsDefault: true,
+}
