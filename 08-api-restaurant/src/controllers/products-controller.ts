2d1a57d30893f93163e9063770562d6de6e72c46
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-03 14:26:37 -0300

    Criando o middleware de tratamento de exceçoes

--- 08-api-restaurant/src/controllers/products-controller.ts
+++ 08-api-restaurant/src/controllers/products-controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 
+import { AppError } from '../utils/AppError'
+
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
