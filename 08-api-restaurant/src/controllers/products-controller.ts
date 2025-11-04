6ae526aa605c550283e8866807be189e4d854645
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-04 13:04:39 -0300

    Validando dados do produto

--- 08-api-restaurant/src/controllers/products-controller.ts
+++ 08-api-restaurant/src/controllers/products-controller.ts
@@ -1,11 +1,26 @@
 import { NextFunction, Request, Response } from 'express'
 
-import { AppError } from '../utils/AppError'
+import { z } from 'zod'
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.status(200).json({ message: 'OK' })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string({ required_error: 'Name is required' }).trim().min(3),
+        price: z.number().gt(0, { message: 'Price must be greater than zero' }),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      return response.status(201).json({ name, price })
     } catch (error) {
       next(error)
     }
