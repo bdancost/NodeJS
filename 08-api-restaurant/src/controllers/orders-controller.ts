7947cdb6d080d594547e62ed3a9ab49312479d47
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-06 16:13:55 -0300

    Rotaspara adicionar pedidopelo preço

--- 08-api-restaurant/src/controllers/orders-controller.ts
+++ 08-api-restaurant/src/controllers/orders-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { AppError } from '@/utils/AppError'
+import { knex } from '@/database/knex'
 import { z } from 'zod'
 
 class OrdersController {
@@ -11,6 +13,31 @@ class OrdersController {
       })
 
       const { table_session_id, product_id, quantity } = bodySchema.parse(request.body)
+
+      const session = await knex<TablesSessionsRepository>('tables_sessions').where({ id: table_session_id }).first()
+
+      if (!session) {
+        throw new AppError('Table session not found.')
+      }
+
+      if (session.closed_at) {
+        throw new AppError('This table is already closed.')
+      }
+
+      const product = await knex<ProductRepository>('products').where({ id: product_id }).first()
+
+      if (!product) {
+        throw new AppError('Product not found.')
+      }
+
+      const price = product.price * quantity
+
+      await knex<OrderRepository>('orders').insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price,
+      })
 
       return response.status(201).json()
     } catch (error) {
