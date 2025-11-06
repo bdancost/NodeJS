d6d221c3253258d848976f0190b7fcd1153137d0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-06 14:16:36 -0300

    Abrindo mesa

--- 08-api-restaurant/src/database/migrations/20251106161021_create-tables-sessions.ts
+++ 08-api-restaurant/src/database/migrations/20251106161021_create-tables-sessions.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.increments('id').primary()
     table.integer('table_id').notNullable().references('id').inTable('tables')
     table.timestamp('opened_at').defaultTo(knex.fn.now())
-    table.timestamp('closed_at')
+    table.time('closed_at')
   })
 }
 
