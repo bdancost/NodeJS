0331d27725f67cc96adcf7b0a3e78798428e5e91
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-18 14:58:36 -0300

    Validando Dados do usuario

--- 10-rocket-log/src/controllers/users-controller.ts
+++ 10-rocket-log/src/controllers/users-controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 class UsersController {
   create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(req.body)
+
     return res.json({ message: 'User created' })
   }
 }
