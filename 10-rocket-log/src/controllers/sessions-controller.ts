6f82dcef024b27ffcf5e0a7ff1712b005d61e203
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-20 18:11:31 -0300

    Validando Variavel de ambiente

--- 10-rocket-log/src/controllers/sessions-controller.ts
+++ 10-rocket-log/src/controllers/sessions-controller.ts
@@ -1,8 +1,42 @@
+import { AppError } from '@/utils/AppError'
 import { Request, Response } from 'express'
+import { authConfig } from '@/configs/auth'
+import { prisma } from '@/database/prisma'
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { z } from 'zod'
 
 class SessionsController {
   async create(req: Request, res: Response) {
-    return res.status(200).json({ message: 'Session created' })
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(req.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({ role: user.role ?? 'customer' }, secret, {
+      subject: user.id,
+      expiresIn,
+    })
+
+    return res.status(200).json({ token })
   }
 }
 
