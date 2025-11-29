dc2fb1a7e2080728a9644a68027e1923f6d77aa2
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-29 18:37:56 -0300

    Validandodados do usuário

--- 11-refund-api/src/controllers/users-controller.ts
+++ 11-refund-api/src/controllers/users-controller.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express'
+import { UserRole } from '@prisma/client'
+import { z } from 'zod'
 
 class UsersController {
   async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3, { message: 'Name must have at least 3 characters' }),
+      email: z.string().trim().email({ message: 'Invalid email' }).toLowerCase(),
+      password: z.string().trim().min(6, { message: 'Password must have at least 6 characters' }),
+      role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee),
+    })
+
+    const { name, email, password, role } = bodySchema.parse(req.body)
+
     res.json({
-      message: 'User created',
+      name,
+      email,
+      password,
+      role,
     })
   }
 }
