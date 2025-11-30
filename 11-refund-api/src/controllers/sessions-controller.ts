7f59d0b3bbcab432b1f3e5fd103999456c4ebcc4
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-11-29 22:03:04 -0300

    Validando dados da sessions

--- 11-refund-api/src/controllers/sessions-controller.ts
+++ 11-refund-api/src/controllers/sessions-controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 class SessionsController {
   async create(req: Request, res: Response) {
-    const { email, password } = req.body
+    const bodySchema = z.object({
+      email: z.string().trim().email({ message: 'E-mail inválido' }).toLowerCase(),
+      password: z.string(),
+    })
+
+    const { email, password } = bodySchema.parse(req.body)
 
     res.json({ email, password })
   }
