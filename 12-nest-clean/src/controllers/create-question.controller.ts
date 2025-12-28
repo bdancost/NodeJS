fc2e42665dc04813a8e962a13e9d60817a5e9808
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-28 12:08:36 -0300

    feat:Protegendo rotas com Guards

---
+++ 12-nest-clean/src/controllers/create-question.controller.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-useless-constructor */
+
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  async handle() {
+    return 'ok'
+  }
+}
