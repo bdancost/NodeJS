caa84eb0fdf58ec6d3a61763c5c5e00d4f6df063
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-29 16:17:21 -0300

    feat:Controller de listagem de perguntas

---
+++ 12-nest-clean/src/controllers/fetch-recent-question.controller.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-useless-constructor */
+
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { Controller, Get, UseGuards, Query } from '@nestjs/common'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: 1,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return {
+      questions,
+    }
+  }
+}
