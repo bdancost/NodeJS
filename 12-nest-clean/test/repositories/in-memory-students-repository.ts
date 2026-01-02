c6fee0202d104941a3f45d91cd43a4b0734f0dc7
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-02 20:57:56 -0300

    test:Testes de cadastro e autenticaçao

---
+++ 12-nest-clean/test/repositories/in-memory-students-repository.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-useless-constructor */
+
+import { Student } from '@/domain/forum/enterprise/entities/student.js'
+import { DomainEvents } from '@/core/events/domain-events.js'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+  public items: Student[] = []
+
+  async findByEmail(email: string) {
+    const student = this.items.find((item) => item.email.toString() === email)
+
+    if (!student) {
+      return null
+    }
+
+    return student
+  }
+
+  async create(student: Student) {
+    this.items.push(student)
+
+    DomainEvents.dispatchEventsForAggregate(student.id)
+  }
+}
