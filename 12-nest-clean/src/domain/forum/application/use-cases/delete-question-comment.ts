9884a74279e55ddbc0e5c1d4e93713e4f61fe025
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-11 22:46:30 -0300

    feat:Controller: Deletar comentario

--- 12-nest-clean/src/domain/forum/application/use-cases/delete-question-comment.ts
+++ 12-nest-clean/src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -4,6 +4,7 @@ import { QuestionCommentsRepository } from '../repositories/question-comments-re
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { left, right, Either } from '@/core/either'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteQuestionCommentUseCaseRequest {
   authorId: string
@@ -15,6 +16,7 @@ type DeleteQuestionCommentUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteQuestionCommentUseCase {
   constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
 
