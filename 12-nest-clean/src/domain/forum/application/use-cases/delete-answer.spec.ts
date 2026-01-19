f05acbc0775ea8332a9951c1390a2982b38fcc16
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-19 17:23:06 -0300

    feat:Respostas com anexos

--- 12-nest-clean/src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ 12-nest-clean/src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -4,7 +4,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
 import { DeleteAnswerUseCase } from './delete-answer.js'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error.js'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachment-repository.js'
-import { makeAnswerAttachment } from 'test/factories/make-answer-attachment.js'
+import { makeAnswerAttachment } from 'test/factories/make-answer-attachments.js'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
---
+++ 12-nest-clean/test/factories/make-answer-attachments.ts
@@ -0,0 +1,44 @@
+import {
+  AnswerAttachment,
+  type AnswerAttachmentProps,
+} from '@/domain/forum/enterprise/entities/answer-attachment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+export function makeAnswerAttachment(
+  override: Partial<AnswerAttachmentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const answerAttachment = AnswerAttachment.create(
+    {
+      answerId: new UniqueEntityID(),
+      attachmentId: new UniqueEntityID(),
+      ...override,
+    },
+    id,
+  )
+
+  return answerAttachment
+}
+
+@Injectable()
+export class AnswerAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerAttachment(
+    data: Partial<AnswerAttachmentProps> = {},
+  ): Promise<AnswerAttachment> {
+    const answerAttachment = makeAnswerAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: answerAttachment.attachmentId.toString(),
+      },
+      data: {
+        answerId: answerAttachment.answerId.toString(),
+      },
+    })
+
+    return answerAttachment
+  }
+}
